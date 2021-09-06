add20bb2e9777f46f49d65d591854dfd2a4b13a6
Ashok K. Majhi <ashokmajhi2work@gmail.com>
2021-09-06 19:19:52 +0530

    Hands on exercise #2,3,4,5 : added and exercise#1 : updated comment

--- exercise1.go
+++ exercise1.go
@@ -1,3 +1,13 @@
+/*
+1. Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+	a. 42
+	b. "James Bond"
+	c. true
+2. Now print the values stored in those variables using
+	a. a single print statement
+	b. multiple print statements
+
+*/
 package main
 
 import "fmt"
